fb10fd48eeaebdcf1d699dc4f96d9e47d941c09b
GiftedR <kidsgamers@hotmail.com>
2023-05-25 20:31:19 -0700

    Updated index to start using side by side page, made logo resize via javascript

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+let brandLogo = document.getElementsByClassName("logo")[0];
+let navFlex = document.getElementsByClassName("navFlex")[0];
+
+brandLogo.style.height = navFlex.clientHeight + "px";
